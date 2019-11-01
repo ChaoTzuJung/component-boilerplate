f538019652be42302afa4caa44c381239ad17883
ChaoTzuJung <s110319022@stu.ntue.edu.tw>
2019-11-01 13:58:22 +0800

    fix: Use code split to react router.

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,18 @@ const routeConfig = [
 		path: '/',
 		component: App,
 		indexRoute: {
-			component: Home
+			// react-router實現按需載入(code split): https://codertw.com/%E5%89%8D%E7%AB%AF%E9%96%8B%E7%99%BC/235905/
+			getComponent(nextState, cb) => require.ensure(
+				// dependencies
+				[],
+				// callback
+				require => {
+					const component = require('./Home').default;
+					cb(null, component);
+				},
+				// chunkName
+				'Home'
+			)
 		},
 		childRoutes: [],
 	}
